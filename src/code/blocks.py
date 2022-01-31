1316debd24a53c6c56e999524cdd0619a6334091
Lennart4711 <lennart.randomthings@gmail.com>
2022-01-31 18:12:19 +0100

    Folder for block images

--- src/code/blocks.py
+++ src/code/blocks.py
@@ -2,30 +2,31 @@ from sprite import Sprite
 import pygame
 
 #grass = pygame.image.load('src\\assets\\default_grass.png').convert()
+path = "src\\assets\\blocks\\"
 
 class Void(Sprite):
     def __init__(self,pos):
         super().__init__(pos)
-        self.img = pygame.image.load('src\\assets\\void.png').convert()
+        self.img = pygame.image.load(path+'void.png').convert()
 
 class Grass(Sprite):
     def __init__(self,pos):
         super().__init__(pos)
-        self.img = pygame.image.load('src\\assets\\default_grass.png').convert()
+        self.img = pygame.image.load(path+'default_grass.png').convert()
 
 class Farmland(Sprite):
     def __init__(self,pos):
         super().__init__(pos)
-        self.img = pygame.image.load('src\\assets\\farmland.png').convert()
+        self.img = pygame.image.load(path+'farmland.png').convert()
 
 class Void(Sprite):
     def __init__(self,pos):
         super().__init__(pos)
-        self.img = pygame.image.load('src\\assets\\void.png').convert()
+        self.img = pygame.image.load(path+'void.png').convert()
 
 class Tower(Sprite):
     def __init__(self,pos):
         super().__init__(pos)
-        self.img = pygame.image.load('src\\assets\\higher.png').convert()
+        self.img = pygame.image.load(path+'higher.png').convert()
 
 
